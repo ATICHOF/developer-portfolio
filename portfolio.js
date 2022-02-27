1ae9a4b3f3e9f806a57b3d24d4849bf5f24668e9
ATICHOF <sof.atiche@gmail.com>
2022-02-27 12:57:21 +0100

    adding portfolio

---
+++ portfolio.js
@@ -0,0 +1,185 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+  name: "ATICHE Soufyane",
+  title: "Hi all, I'm ATICHOF",
+  description:
+    "A passionate Full Stack Web Developer and Mobile App Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs / ReactNative / Django and some other cool libraries and frameworks and Cross Platform Mobile Apps With Flutter.",
+  resumeLink:
+    "https://drive.google.com/file/d/1mPp2WMfo7D1E-Cd5QPh2DUBVT6O2dzoy/view?usp=sharing",
+};
+
+export const openSource = {
+  githubUserName: "atichof",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  facebook: "https://www.facebook.com/soufyane.atiche/",
+  instagram: "https://www.instagram.com/atich0f/",
+  twitter: "https://twitter.com/sof_atiche",
+  github: "https://github.com/atichof",
+  linkedin: "https://www.linkedin.com/in/soufyane-atiche-75060717a/",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications "),
+    emoji(
+      "⚡ Integration of third party services such as Firebase and ViroReact"
+    ),
+  ],
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    {
+      skillName: "PHP",
+      fontAwesomeClassname: "vscode-icons:file-type-php",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "vscode-icons:file-type-sql",
+    },
+    {
+      skillName: "yarn",
+      fontAwesomeClassname: "vscode-icons:file-type-yarn",
+    },
+    {
+      skillName: "reactNative",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "logos:firebase",
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "logos:python",
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "logos:docker-icon",
+    },
+  ],
+};
+
+export const SkillBars = [
+  {
+    Stack: "Frontend/Design",
+    progressPercentage: "90",
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "60",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "60",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "Sidi Ahmed Bennacer HighSchool",
+    subHeader: "Bachelor in Mathematics",
+    duration: "September 2017",
+  },
+  {
+    schoolName: "CPGE IBN GHAZI",
+    subHeader: "MPSI",
+    duration: "September 2018 - April 2020",
+    desc: "Classe Préparatoire option MPSI",
+  },
+  {
+    schoolName: "INSEA",
+    subHeader: "National Institute of Statistics and Applied Economie",
+    duration: "September 2020 - April 2022",
+    desc: "Studied Data & Software Engineering",
+  },
+];
+
+export const experience = [
+  {
+    role: "Software Engineer internship",
+    company: "Renault Group",
+    companylogo: "/img/icons/common/google.svg",
+    date: "July 2021 – August 2021",
+    desc: "the Development of a Web Application using React Native and Viro React to add some Augmented Reality Functionalities",
+    descBullets: ["ReactNative", "Java Spring Boot", "Viro React"],
+  },
+];
+
+export const projects = [
+  {
+    name: "Fast Cast",
+    desc: " Platforme de Collection des Videos, devlopper en react, dans laquelle vous pouvez uploader et sauvgarder vos videos personnel: Solution en cours de developpement",
+    github: "https://github.com/ATICHOF/insea-workshop-front",
+    link: "http://insea-workshop-front-ten.vercel.app/",
+  },
+  {
+    name: " Taches-Note",
+    desc: "Application Mobile de gestion des taches en Java",
+    github: "https://github.com/ATICHOF/Taches-Note",
+  },
+  {
+    name: "CRUDBooks",
+    desc: "Librairie ou vous pouvez ajouter,Supprimer,Modifier les nomes des livres que vous preferer",
+    github: "https://github.com/ATICHOF/CRUDBooks",
+  },
+  {
+    name: "PAMProjet",
+    desc: " projet Scolaire : site web des Plantes Aromatiques et Medicinales",
+    github: "https://github.com/ATICHOF/PAMProjet",
+  },
+  {
+    name: "Restaurant Confusion",
+    desc: "Site Web du Restaurant Confusion : Projet de certification Coursera En React",
+    github: "https://github.com/ATICHOF/confusion",
+  },
+  {
+    name: "Analyse D'absence",
+    desc: "Projet Scolaire de DataWareHouse : Analyse et traitement d'absnece des Etudiants de L'INSEA en utilison Appache Airflow ,jupyter Notebook, Postgres, et PowerBI pour visualisation des Resultats",
+    github: "https://github.com/ATICHOF/confusion",
+  },
+  {
+    name: "R-EYE",
+    desc: "Realisation de l'application R-EYE d'aide de Commercialisation des Voitures En Fonction de la Realite Augmentee : Donnee Privee",
+  },
+];
